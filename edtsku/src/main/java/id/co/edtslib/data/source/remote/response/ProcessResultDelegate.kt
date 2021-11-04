8d1522d45c36a0134186c4f31eeff37eaa084dd4
tonikustiana <toni.kustiana@sg-dsa.com>
2021-11-04 12:02:29 +0700

    add auth message

--- edtsku/src/main/java/id/co/edtslib/data/source/remote/response/ProcessResultDelegate.kt
+++ edtsku/src/main/java/id/co/edtslib/data/source/remote/response/ProcessResultDelegate.kt
@@ -3,7 +3,7 @@ package id.co.edtslib.data.source.remote.response
 interface ProcessResultDelegate<T> {
     fun loading()
     fun error(code: String?, message: String?)
-    fun unAuthorize()
+    fun unAuthorize(message: String?)
     fun success(data: T?)
     fun errorConnection()
     fun errorSystem()
