df18f27a411237e55d555d2f8bf67d0d093f74c5
tonikustiana <toni.kustiana@sg-dsa.com>
2021-11-17 05:44:06 +0700

    remove timber

--- app/src/main/java/id/co/edts/edtsku/example/App.kt
+++ app/src/main/java/id/co/edts/edtsku/example/App.kt
@@ -4,14 +4,18 @@ import android.app.Application
 import id.co.edts.edtsku.example.di.appModule
 import id.co.edts.edtsku.example.di.repositoryModule
 import id.co.edts.edtsku.example.di.viewModelModule
+import id.co.edtslib.BuildConfig
 import id.co.edtslib.di.EdtsKu
+import timber.log.Timber
 
 class App: Application() {
 
     override fun onCreate() {
         super.onCreate()
 
-        EdtsKu.init(this, "http://api.edtsku.com",
+        Timber.plant(Timber.DebugTree())
+
+        EdtsKu.init(this, "http://emart-dev.elevenia.co.id",
             listOf(appModule, repositoryModule, viewModelModule))
     }
 }
