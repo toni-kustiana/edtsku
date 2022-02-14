3c3949516df4e5d7178eae01643fc8faf4ebdd74
tonikustiana <toni.kustiana@sg-dsa.com>
2022-02-14 23:22:35 +0700

    change ripple style

--- app/src/main/java/id/co/edts/edtsku/example/di/AppModule.kt
+++ app/src/main/java/id/co/edts/edtsku/example/di/AppModule.kt
@@ -5,7 +5,7 @@ import org.koin.dsl.module
 import retrofit2.Retrofit
 
 val appModule = module {
-    single { provideApiService(get(named("edtsku"))) }
+    single { provideApiService(get(named("api"))) }
 }
 
 private fun provideApiService(retrofit: Retrofit): ApiService =
