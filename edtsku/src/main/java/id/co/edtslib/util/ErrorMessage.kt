6007f6f6f246576345c3dd1fbf7ace2139143c41
tonikustiana <toni.kustiana@sg-dsa.com>
2022-05-31 10:03:47 +0700

    move unknown error to common error

--- edtsku/src/main/java/id/co/edtslib/util/ErrorMessage.kt
+++ edtsku/src/main/java/id/co/edtslib/util/ErrorMessage.kt
@@ -3,5 +3,5 @@ package id.co.edtslib.util
 open class ErrorMessage {
     open fun http503() = "Sedang ada gangguan sistem, coba lagi lain waktu."
     open fun connection() = "Koneksi sedang bermasalah, cek kembali koneksimu."
-    open fun system(message: String?) = "Terjadi kesalahan sistem. ($message)"
+    open fun system(message: String?) = "Terjadi kesalahan. ($message)"
 }
