b66528eb7962a8b5a0ca041e756197eaf41aa87c
tonikustiana <toni.kustiana@sg-dsa.com>
2022-06-14 16:07:09 +0700

    add example

--- app/src/main/java/id/co/edts/edtsku/example/MainActivity.kt
+++ app/src/main/java/id/co/edts/edtsku/example/MainActivity.kt
@@ -1,8 +1,8 @@
 package id.co.edts.edtsku.example
 
 import android.content.Intent
-import android.net.Uri
 import android.view.LayoutInflater
+import androidx.core.content.FileProvider
 import id.co.edts.edtsku.example.databinding.ActivityMainBinding
 import id.co.edtslib.uibase.BaseActivity
 import org.koin.androidx.viewmodel.ext.android.viewModel
@@ -19,8 +19,14 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
        configurationViewModel.download("https://qlarp-api-dev.arsenadevelopment.eu/test.pdf").observe(this) {
            if (it != null) {
                val file = File(it)
+               val uri = FileProvider.getUriForFile(
+                   this,
+                   "${packageName}.provider",
+                   file
+               )
+
                val intent = Intent(Intent.ACTION_VIEW)
-               intent.setDataAndType(Uri.fromFile(file), "application/pdf")
+               intent.data = uri
                intent.flags = Intent.FLAG_ACTIVITY_NO_HISTORY
                startActivity(intent)
            }
