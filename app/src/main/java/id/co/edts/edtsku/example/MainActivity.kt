2b6510e4d808b75dd248d75ad96e6bc6411e9d54
tonikustiana <toni.kustiana@sg-dsa.com>
2022-09-26 00:13:17 +0700

    add popuactivity

--- app/src/main/java/id/co/edts/edtsku/example/MainActivity.kt
+++ app/src/main/java/id/co/edts/edtsku/example/MainActivity.kt
@@ -1,41 +1,17 @@
 package id.co.edts.edtsku.example
 
-import android.content.Intent
 import android.view.LayoutInflater
-import android.webkit.MimeTypeMap
-import androidx.core.content.FileProvider
 import id.co.edts.edtsku.example.databinding.ActivityMainBinding
 import id.co.edtslib.uibase.BaseActivity
-import org.koin.androidx.viewmodel.ext.android.viewModel
-import java.io.File
 
 
 class MainActivity : BaseActivity<ActivityMainBinding>() {
-    private val configurationViewModel: ConfigurationViewModel by viewModel()
 
     override val bindingInflater: (LayoutInflater) -> ActivityMainBinding
         get() = ActivityMainBinding::inflate
 
     override fun setup() {
-       configurationViewModel.download("https://qlarp-api-dev.arsenadevelopment.eu/test.pdf").observe(this) {
-           if (it?.file != null) {
-               val file = File(it.file!!)
-               val uri = FileProvider.getUriForFile(
-                   this,
-                   "${packageName}.provider",
-                   file
-               )
 
-               val extension = MimeTypeMap.getFileExtensionFromUrl(it.file)
-               if (extension != null) {
-
-                   val intent = Intent(Intent.ACTION_VIEW)
-                   intent.flags = Intent.FLAG_GRANT_READ_URI_PERMISSION
-                   intent.setDataAndType(uri, it.type)
-                   startActivity(intent)
-               }
-           }
-       }
     }
 
     override fun getTrackerPageName() = 0
---
+++ edtsku/src/main/java/id/co/edtslib/uibase/PopupActivity.kt
@@ -0,0 +1,31 @@
+package id.co.edtslib.uibase
+
+import android.graphics.drawable.ColorDrawable
+import android.view.MenuItem
+import androidx.core.content.ContextCompat
+import androidx.viewbinding.ViewBinding
+import id.co.edtslib.R
+
+abstract class PopupActivity<viewBinding: ViewBinding>: BaseActivity<viewBinding>() {
+    override fun canBack() = false
+
+    abstract fun setupPopup()
+    override fun setup() {
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        val actionBar = supportActionBar
+        val colorDrawable = ColorDrawable(ContextCompat.getColor(this, getStatusBarColor()))
+        actionBar?.setBackgroundDrawable(colorDrawable)
+
+        setupPopup()
+    }
+
+    protected open fun getStatusBarColor() = R.color.actionBarColor
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            android.R.id.home -> onBackPressedDispatcher.onBackPressed()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
