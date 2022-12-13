8d24a029d36a4f36ad942c559bcf801cec6ce14a
Hezbi Islami <muhamad.hezbi@sg-edts.com>
2022-12-13 14:50:22 +0700

    remove getTotalPages on contentResponse

--- edtsku/src/main/java/id/co/edtslib/data/source/remote/response/ContentResponse.kt
+++ edtsku/src/main/java/id/co/edtslib/data/source/remote/response/ContentResponse.kt
@@ -2,18 +2,17 @@ package id.co.edtslib.data.source.remote.response
 
 import com.google.gson.annotations.SerializedName
 
-data class ContentResponse<T> (
+data class ContentResponse<T>(
     @SerializedName("content")
     val content: T?,
     @SerializedName("pageable")
     val pageable: PageableDataResponse?,
     @SerializedName("totalPages")
-    val totalPages: Int?,
+    val totalPages: Int,
     @SerializedName("additionalData")
     val additionalData: Map<String, Any>?
 ) {
-    fun getTotalPages() = totalPages ?: 0
     fun getPageNumber() = pageable?.pageNumber ?: 0
     private fun getPageSize(): Int = pageable?.pageSize ?: 0
-    fun getOffset() = getPageSize()*getPageNumber()
+    fun getOffset() = getPageSize() * getPageNumber()
 }
