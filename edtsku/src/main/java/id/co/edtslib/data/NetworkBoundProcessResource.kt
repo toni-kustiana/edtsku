240dca5d079f4c9971726d99e0d74bf23031e55b
tonikustiana <toni.kustiana@sg-dsa.com>
2022-12-21 18:26:26 +0700

    fix refresh token

--- edtsku/src/main/java/id/co/edtslib/data/NetworkBoundProcessResource.kt
+++ edtsku/src/main/java/id/co/edtslib/data/NetworkBoundProcessResource.kt
@@ -31,6 +31,7 @@ abstract class NetworkBoundProcessResource<ResultType, RequestType>(
             Result.Status.UNAUTHORIZED -> {
                 val cachedHeaders = localDataSource.getCached()
                 val refreshToken = cachedHeaders?.get("refresh-token")
+                localDataSource.setBearerToken(refreshToken)
                 if (refreshToken != null) {
                     val tokenResponse = sessionRemoteDataSource.refreshToken(refreshToken)
                     when (tokenResponse.status) {
